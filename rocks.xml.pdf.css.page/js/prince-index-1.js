0db30ea7202a884d9882cbc96b88b364228e17e1
vkushnir <volodya.kr@gmail.com>
2016-05-17 17:27:23 +0300

    Added support for PrinceXML.

---
+++ rocks.xml.pdf.css.page/js/prince-index-1.js
@@ -0,0 +1,36 @@
+var refs = {};
+
+function index(href, counter) {
+    refs[href] = counter;
+    return counter;
+}
+
+function dumpIndex() {
+    console.log("var refs = {");
+
+    var keys = [];
+
+    var index = document.getElementById("index-chapter");
+    var anchors = index.getElementsByTagName("a");
+
+    for (var i = 0; i < anchors.length; ++i) {
+        var anchor = anchors[i];
+        var href = anchor.getAttribute("href");
+
+        if (refs[href]) {
+            keys.push(href);
+        }
+    }
+
+    keys.sort();
+
+    for (i in keys) {
+        var comma = (i < keys.length - 1 ? "," : "");
+        console.log("\"" + keys[i] + "\": " + refs[keys[i]] + comma);
+    }
+
+    console.log("};");
+}
+
+Prince.addScriptFunc("index", index);
+Prince.addEventListener("complete", dumpIndex, false);
