817d82759c1184b8348a08b5fafef213b83280b2
vkushnir <volodya.kr@gmail.com>
2016-05-18 15:07:05 +0300

    Hotfix for input files without Index terms.

--- rocks.xml.pdf.css.page/js/prince-index-2.js
+++ rocks.xml.pdf.css.page/js/prince-index-2.js
@@ -2,34 +2,36 @@ window.onload = rewriteIndex;
 
 function rewriteIndex() {
     var index = document.getElementById("index-chapter");
-    var paras = index.getElementsByTagName("p");
+    if (index) {
+        var paras = index.getElementsByTagName("p");
 
-    for (var i = 0; i < paras.length; ++i) {
-        var oldParagraph = paras[i];
-        var newParagraph = document.createElement("p");
+        for (var i = 0; i < paras.length; ++i) {
+            var oldParagraph = paras[i];
+            var newParagraph = document.createElement("p");
 
-        newParagraph.appendChild(oldParagraph.childNodes[0]);
+            newParagraph.appendChild(oldParagraph.childNodes[0]);
 
-        var pages = {};
-        var childNodes = oldParagraph.getElementsByTagName("a");
-        for (var j = 0; j < childNodes.length; ++j) {
-            var anchor = childNodes[j];
-            var href = anchor.getAttribute("href");
+            var pages = {};
+            var childNodes = oldParagraph.getElementsByTagName("a");
+            for (var j = 0; j < childNodes.length; ++j) {
+                var anchor = childNodes[j];
+                var href = anchor.getAttribute("href");
 
-            var pageNum = refs[href];
-            if (pageNum) {
-                if (!pages[pageNum]) {
-                    newParagraph.appendChild(anchor);
-                    newParagraph.appendChild(document.createTextNode(", "));
+                var pageNum = refs[href];
+                if (pageNum) {
+                    if (!pages[pageNum]) {
+                        newParagraph.appendChild(anchor);
+                        newParagraph.appendChild(document.createTextNode(", "));
 
-                    pages[pageNum] = true;
+                        pages[pageNum] = true;
+                    }
                 }
             }
+
+            newParagraph.removeChild(newParagraph.lastChild);
+
+            var parent = oldParagraph.parentNode;
+            parent.replaceChild(newParagraph, oldParagraph);
         }
-
-        newParagraph.removeChild(newParagraph.lastChild);
-
-        var parent = oldParagraph.parentNode;
-        parent.replaceChild(newParagraph, oldParagraph);
     }
 }
